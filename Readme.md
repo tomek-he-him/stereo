a9f7379b6f8a008a6fd7d1ca836dcb1a284eb277
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-16 00:21:43 +0200

    Bootstrap project

---
+++ Readme.md
@@ -0,0 +1,44 @@
+[![Travis – build status](https://img.shields.io/travis/tomekwi/…/master.svg?style=flat-square)](https://travis-ci.org/tomekwi/…)
+ 
+[![Code climate](https://img.shields.io/codeclimate/github/tomekwi/….svg?style=flat-square)](https://codeclimate.com/github/tomekwi/…)
+ 
+[![David – status of dependencies](https://img.shields.io/david/tomekwi/….svg?style=flat-square)](https://david-dm.org/tomekwi/…)
+ 
+[![Code style: airbnb](https://img.shields.io/badge/code%20style-airbnb-blue.svg?style=flat-square)](https://github.com/airbnb/javascript)
+
+
+
+
+…
+===
+
+**…**
+
+
+
+
+Installation
+------------
+
+```sh
+$ …
+```
+
+
+
+
+Usage
+-----
+
+…
+
+
+
+
+License
+-------
+
+[MIT][] © [Tomek Wiszniewski][]
+
+[MIT]: ./License.md
+[Tomek Wiszniewski]: https://github.com/tomekwi
---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var del = require('del');
+
+var gulp = require('gulp-help')(require('gulp'));
+var babel = require('gulp-babel');
+
+// `gulp build`
+// -----------------------------------------------------------------------------
+gulp.task('build',
+  'Compile everything.',
+  ['scripts']
+);
+
+// `gulp scripts`
+// -----------------------------------------------------------------------------
+var scripts = {
+  source: 'source/**/*.js',
+  target: '.'
+};
+
+gulp.task('scripts',
+  'Compile scripts to ES5.',
+  ['scripts:clean'],
+  function() {
+    return gulp.src(scripts.source)
+      .pipe(babel())
+      .pipe(gulp.dest(scripts.target))
+    ;
+  }
+);
+
+gulp.task('scripts:clean', false, function(done) {
+  del(scripts.target + '/*.{js,js.map}', done);
+});
+
+// `gulp clean`
+// -----------------------------------------------------------------------------
+gulp.task('clean',
+  'Remove all built files.',
+  ['scripts:clean']
+);
