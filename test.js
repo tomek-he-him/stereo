bd8df6342a7ffa00be4f4936fadcbedc2d14e940
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-16 20:12:18 +0200

    Lay out tests

--- test.js
+++ test.js
@@ -1,1 +1,19 @@
 import test from 'tape-catch';
+
+import stereo from './source/index';
+
+test.skip('Creates a new emitter.', (is) => {
+  is.end();
+});
+
+test.skip('Works with single-channel events.', (is) => {
+  is.end();
+});
+
+test.skip('Works with multi-channel events.', (is) => {
+  is.end();
+});
+
+test.skip('Handles errors alright.', (is) => {
+  is.end();
+});
