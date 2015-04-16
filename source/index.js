776b231c59f843c6ddd99ff5b172537bd9097818
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-16 22:02:27 +0200

    Implement .on

--- source/index.js
+++ source/index.js
@@ -1,10 +1,33 @@
-let emitterProto = {
-  on(event, listener) {},
-  once(event, listener) {},
-  off(event, listener) {},
-  emit(event, listener) {}
-};
-
 export default function stereo () {
-  return Object.create(emitterProto);
+  let listeners = {};
+
+  return {
+    on(events, listener) {
+      // Normalize arguments.
+      if (typeof events === 'string') events = [events];
+      else if (!Array.isArray(events)) throw new TypeError(
+        'stereo: The first argument to `.on()` should be `{String} event` or ' +
+        '`{Array} events`.'
+      );
+      if (typeof listener !== 'function') throw new TypeError(
+        'stereo: The second argument to `.on()` should be `{Function} '+
+        'listener`.'
+      );
+
+      // Register the listener.
+      for(let event of events) {
+        let register = listeners[event];
+        if (!register) listeners[event] = [listener];
+        else if (!register.includes(listener)) {
+          register.push(listener);
+        }
+      }
+    },
+
+    once() {},
+
+    off() {},
+
+    emit() {}
+  };
 }
