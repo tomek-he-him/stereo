8238ae702d4f9e9b16508424a4cf5a96b4cb4644
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-16 20:59:24 +0200

    Define the prototype

--- source/index.js
+++ source/index.js
@@ -1,2 +1,10 @@
+let emitterProto = {
+  on(event, listener) {},
+  once(event, listener) {},
+  off(event, listener) {},
+  emit(event, listener) {}
+};
+
 export default function stereo () {
-  }
+  return Object.create(emitterProto);
+}
