0e38c83c8226db97924589c7199d5cd4657cef45
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-16 22:54:38 +0200

    Fox test numbering for good

--- test.js
+++ test.js
@@ -53,45 +53,45 @@ test('Works with single-channel events.', (is) => {
   ));
   emitter.emit('1');
 
-  emitter.on('2 and 3', (has, really) => is.ok(has.passed && really,
+  emitter.on('2', (has, really) => is.ok(has.passed && really,
     'when a listener has arguments'
   ));
-  emitter.emit('2 and 3', {passed: true}, 'yup');
+  emitter.emit('2', {passed: true}, 'yup');
 
-  emitter.on('4 and 5', () => is.pass(
+  emitter.on('3 and 4', () => is.pass(
     'when an event occurs more than once'
   ));
-  emitter.emit('4 and 5');
-  emitter.emit('4 and 5');
+  emitter.emit('3 and 4');
+  emitter.emit('3 and 4');
 
   let five = (firstCall) => is.ok(firstCall,
     '`off` unregisters a specific listener'
   );
-  emitter.on('6 and 7', five);
-  emitter.emit('6 and 7', true);
-  emitter.on('6 and 7', () => is.pass(
+  emitter.on('5 and 6', five);
+  emitter.emit('5 and 6', true);
+  emitter.on('5 and 6', () => is.pass(
     '– really, only a specific one'
   ));
-  emitter.off('6 and 7', five);
-  emitter.emit('6 and 7', false);
+  emitter.off('5 and 6', five);
+  emitter.emit('5 and 6', false);
 
-  emitter.on('8 and 9', (firstCall) => is.ok(firstCall,
+  emitter.on('7 and 8', (firstCall) => is.ok(firstCall,
     '`off` unregisters all listeners on a channel'
   ));
-  emitter.on('8 and 9', (firstCall) => is.ok(firstCall,
+  emitter.on('7 and 8', (firstCall) => is.ok(firstCall,
     '– no matter how many'
   ));
-  emitter.emit('8 and 9', true);
-  emitter.off('8 and 9');
-  emitter.emit('8 and 9', false);
+  emitter.emit('7 and 8', true);
+  emitter.off('7 and 8');
+  emitter.emit('7 and 8', false);
 
-  emitter.on('10', (has) => is.ok(has.passed,
+  emitter.on('9 and 10', (has) => is.ok(has.passed,
     'passes arguments to all listeners'
   ));
-  emitter.on('10', (has) => is.ok(has.passed,
+  emitter.on('9 and 10', (has) => is.ok(has.passed,
     '– even if there are more of them'
   ));
-  emitter.emit('10', {passed: true});
+  emitter.emit('9 and 10', {passed: true});
 
   emitter.once('11', (firstCall) => is.ok(firstCall,
     '`once` fires listeners only once'
