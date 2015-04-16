40d09ee1e24ef89f126416d87d44e8cafe6b706e
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-16 21:34:56 +0200

    Specify single-channel emitter

--- test.js
+++ test.js
@@ -43,7 +43,62 @@ test('The API is in good shape.', (is) => {
   is.end();
 });
 
-test.skip('Works with single-channel events.', (is) => {
+test('Works with single-channel events.', (is) => {
+  is.plan(10);
+
+  let emitter = stereo();
+
+  emitter.on('1', () => is.pass(
+    'in the simplest case'
+  ));
+  emitter.emit('1');
+
+  emitter.on('2', (has, really) => is.ok(has.passed && really,
+    'when a listener has arguments'
+  ));
+  emitter.emit('2', {passed: true}, 'yup');
+
+  emitter.on('3 and 4', () => is.pass(
+    'when an event occurs more than once'
+  ));
+  emitter.emit('3 and 4');
+  emitter.emit('3 and 4');
+
+  let five = (firstCall) => is.ok(firstCall,
+    'with `off` unregistering a specific listener'
+  );
+  emitter.on('5 and 6', five);
+  emitter.emit('5 and 6', true);
+  emitter.on('5 and 6', () => is.pass(
+    '– really, only a specific one'
+  ));
+  emitter.off('5 and 6', five);
+  emitter.emit('5 and 6', false);
+
+  emitter.on('7 and 8', (firstCall) => is.ok(firstCall,
+    'with `off` unregistering all listeners on a channel'
+  ));
+  emitter.on('7 and 8', (firstCall) => is.ok(firstCall,
+    '– no matter how many'
+  ));
+  emitter.emit('7 and 8', true);
+  emitter.off('7 and 8');
+  emitter.emit('7 and 8', false);
+
+  emitter.on('9', (has) => is.ok(has.passed,
+    'passes arguments to all listeners'
+  ));
+  emitter.on('9', (has) => is.ok(has.passed,
+    '– even if there are more of them'
+  ));
+  emitter.emit('9', {passed: true});
+
+  emitter.once('10', (firstCall) => is.ok(firstCall,
+    'firing listeners only once when they’re registered with `once`'
+  ));
+  emitter.emit('10', true);
+  emitter.emit('10', false);
+
   is.end();
 });
 
