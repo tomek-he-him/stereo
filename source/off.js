3810c977705e7a5c98d3eee129f3a23b4edc1ec6
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-21 00:36:03 +0200

    Cache listeners

--- source/off.js
+++ source/off.js
@@ -2,22 +2,25 @@ import normalizeEvents from './tools/normalizeEvents';
 import normalizeListener from './tools/normalizeListener';
 import getListeners from './tools/getListeners';
 
-export default (emitter) => function off(events, listener) {
+export default (emitter) => {
   let listeners = getListeners(emitter);
 
-  // Normalize arguments.
-  events = normalizeEvents(events);
+  return function off(events, listener) {
 
-  // If no listener is specified, unregister all listeners.
-  if (listener == null) for (let event of events) {
-    delete listeners[event];
-  }
+    // Normalize arguments.
+    events = normalizeEvents(events);
 
-  // Otherwise unregister the given listener.
-  else {
-    listener = normalizeListener(listener);
-    for (let event of events) {
-      listeners[event].delete(listener);
+    // If no listener is specified, unregister all listeners.
+    if (listener == null) for (let event of events) {
+      delete listeners[event];
     }
-  }
+
+    // Otherwise unregister the given listener.
+    else {
+      listener = normalizeListener(listener);
+      for (let event of events) {
+        listeners[event].delete(listener);
+      }
+    }
+  };
 };
--- source/on.js
+++ source/on.js
@@ -2,17 +2,20 @@ import normalizeEvents from './tools/normalizeEvents';
 import normalizeListener from './tools/normalizeListener';
 import getListeners from './tools/getListeners';
 
-export default (emitter) => function on(events, listener) {
+export default (emitter) => {
   let listeners = getListeners(emitter);
 
-  // Normalize arguments.
-  events = normalizeEvents(events);
-  listener = normalizeListener(listener);
+  return function on(events, listener) {
 
-  // Register the listener.
-  for (let event of events) {
-    let register = listeners[event];
-    if (!register) listeners[event] = new Set([listener]);
-    else if (!register.has(listener)) register.add(listener);
-  }
+    // Normalize arguments.
+    events = normalizeEvents(events);
+    listener = normalizeListener(listener);
+
+    // Register the listener.
+    for (let event of events) {
+      let register = listeners[event];
+      if (!register) listeners[event] = new Set([listener]);
+      else if (!register.has(listener)) register.add(listener);
+    }
+  };
 };
