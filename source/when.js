3666c6a4795fa6549dd3160d2f998273c909faf0
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-21 21:46:22 +0200

    Implement a kit version of `.when()`

--- source/when.js
+++ source/when.js
@@ -1,22 +1,28 @@
 import normalizeEvents from './tools/normalizeEvents';
 import normalizeListener from './tools/normalizeListener';
+import hookListener from './tools/hookListener';
 
 export default (emit) => {
-  let { cache } = emit;
+  // Hook the event cache to `.emit()`.
+  let cache = {};
+  emit.hooks.push((event, args) => {
+    cache[event] = args;
+  });
+
+  let hook = hookListener(emit.listeners);
 
   return function when(events, listener) {
-    let { on } = emit;
     events = normalizeEvents(events);
     listener = normalizeListener(listener);
 
     // If any of the `events` is in the cache, trigger the listener.
-    let eventCache;
-    for (let event of events) if ((eventCache = cache[event])) {
-      listener(eventCache.latest);
+    let args;
+    for (let event of events) if ((args = cache[event])) {
+      listener(...args);
       break;
     }
 
-    // Register through `on`.
-    on(events, listener);
+    // Hook the listener to the `events`.
+    hook(events, listener);
   };
 };
